8a741f3d4d1373dc032bcfe555ffda3fe7f07f40
Ibraim Medina <ibraimedina@gmail.com>
2017-02-04 16:19:50 -0200

    Add material-ui style

--- src/app/app.js
+++ src/app/app.js
@@ -1,33 +1,43 @@
 import React, { Component } from 'react';
+import { Center, Page, VBox } from 'react-layout-components';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
-import {Card, CardActions, CardHeader, CardText} from 'material-ui/Card';
-import FlatButton from 'material-ui/FlatButton';
+import { Card, CardActions, CardHeader, CardText } from 'material-ui/Card';
+import RaisedButton from 'material-ui/RaisedButton';
 import TextField from 'material-ui/TextField';
+import AppDefaultTheme from '../theme/theme';
+
+// Needed for onTouchTap
+// http://stackoverflow.com/a/34015469/988941
+import injectTapEventPlugin from 'react-tap-event-plugin';
+injectTapEventPlugin();
+
+
 
 function Login(props) {
   return (
-      <Card>
-        <CardHeader title="Login" />
-        <CardText>
-          <div>
-            <TextField hintText="username"/>
-          </div>
-          <div>
-            <TextField hintText="password" type="password"/>
-          </div>
-        </CardText>
-        <CardActions>
-          <FlatButton disabled label="Login"/>
-        </CardActions>
-      </Card>
+    <Card>
+      <CardHeader title="Enter SSPES"/>
+      <CardText><VBox>
+          <TextField hintText="username"/>
+          <TextField hintText="password" type="password"/>
+      </VBox></CardText>
+      <CardActions style={{float:"right"}}>
+        <RaisedButton primary label="Login"/>
+      </CardActions>
+    </Card>
   );
 }
+
 class App extends Component {
   render = ()  => (
-      <MuiThemeProvider>
-        <Login/>
-      </MuiThemeProvider>
-    );
+    <MuiThemeProvider muiTheme={AppDefaultTheme}>
+      <Page>
+        <Center height={"100%"}>
+          <Login/>
+        </Center>
+      </Page>
+    </MuiThemeProvider>
+  );
 }
 
 export default App;
